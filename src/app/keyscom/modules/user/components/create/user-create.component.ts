b1304fecf2bb2d7e14b09910efb6202475c4cb28
Fran <fponce@clicars.com>
2022-04-02 19:50:00 +0200

    new directory tree

--- src/app/keyscom/modules/user/components/create/user-create.component.ts
+++ src/app/keyscom/modules/user/components/create/user-create.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-user-main',
@@ -6,6 +7,13 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./user-create.component.scss'],
 })
 export class UserCreateComponent implements OnInit {
+  constructor(private router: Router) {}
+
   ngOnInit(): void {
+    console.log('test');
+  }
+
+  public saveUser(): void {
+    this.router.navigate(['/user']);
   }
 }
