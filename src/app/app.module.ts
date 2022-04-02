f17d45ee6486cf39d626c65d4967f3afd87b266d
Fran <fponce@clicars.com>
2022-04-02 13:36:10 +0200

    add interceptor on keyscom module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,10 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import {JwtInterceptor} from './keyscom/modules/auth/services/jwt.interceptor';
+import {HttpClientModule} from '@angular/common/http';
+import {KeyscomModule} from './keyscom/keyscom.module';
+import {AppComponent} from './app.component';
 
 @NgModule({
   declarations: [
@@ -16,14 +16,8 @@ import {JwtInterceptor} from './keyscom/modules/auth/services/jwt.interceptor';
     HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    KeyscomModule,
   ],
   bootstrap: [AppComponent],
-  providers: [
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: JwtInterceptor,
-      multi: true,
-    },
-  ],
 })
 export class AppModule { }
--- src/app/keyscom/keyscom.module.ts
+++ src/app/keyscom/keyscom.module.ts
@@ -3,6 +3,8 @@ import {KeyscomRoutingModule} from './keyscom-routing.module';
 import {UserModule} from './modules/user/user.module';
 import {AuthModule} from './modules/auth/auth.module';
 import {DashboardModule} from './modules/dashboard/dashboard.module';
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
+import {JwtInterceptor} from './modules/auth/services/jwt.interceptor';
 
 @NgModule({
   imports: [
@@ -12,6 +14,13 @@ import {DashboardModule} from './modules/dashboard/dashboard.module';
   ],
   exports: [
     KeyscomRoutingModule,
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: JwtInterceptor,
+      multi: true,
+    },
   ],
 })
 export class KeyscomModule {}
